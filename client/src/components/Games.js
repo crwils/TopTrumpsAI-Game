51f8a3ef701eca9c4ec488f6208105365cc9e152
crispymccoy145 <samspam19941@gmail.com>
2021-05-22 12:14:26 +0100

    player One hand created, not randomised

--- client/src/components/Games.js
+++ client/src/components/Games.js
@@ -2,9 +2,26 @@ import React from 'react';
 import CardItem from './CardItem';
 
 const Cards = ({cards}) => {
-    const cardItems = cards.map((card, index) => {
+
+    const playerOneCards = []
+    const playerTwoCards = []
+
+    cards.map((card, index) => {
+        if(index === 0 || index%2 === 0){
+            return playerOneCards.push(card)
+        }else{
+            return playerTwoCards
+        }
+    });
+
+    const cardItems = playerOneCards.map((card, index) => {
         return <CardItem card={card} key={index}/>
     })
+    // const cardItems = cards.map((card, index) => {
+    //     return <CardItem card={card} key={index}/>
+    // })
+
+
 
     return (
         <ul>
