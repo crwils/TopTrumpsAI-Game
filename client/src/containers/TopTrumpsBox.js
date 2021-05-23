28289060fc1fee38325bca6863c565265db93dbd
klamb95 <klamb1995@gmail.com>
2021-05-23 16:10:32 +0100

    ended for the day

--- client/src/containers/TopTrumpsBox.js
+++ client/src/containers/TopTrumpsBox.js
@@ -65,7 +65,7 @@ function TopTrumpsBox(){
 
             tempDrawArray.push(player1Array[0])
             tempDrawArray.push(player2Array[0])
-            setDrawArray(tempDrawArray)
+            setDrawArray(...drawArray, tempDrawArray) // 
 
             player2Array.splice(indexNumber, 1)
             player1Array.splice(indexNumber, 1)
