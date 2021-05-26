faefcc9f84a5fae387590963e0416fa60825fd83
Craig <craigwilsonmusic@gmail.com>
2021-05-26 15:48:13 +0100

    round score display done in css

--- client/src/components/Games.js
+++ client/src/components/Games.js
@@ -5,7 +5,7 @@ import Player2CardItem from './Player2CardItem'
 
 import databaseAttributeTranslator from '../containers/TopTrumpsBox';
 
-const Games = ({ flip, setFlip, playerWins, isFlipped, setIsFlipped, drawArray, cards, playerOneCards, playerTwoCards, setPlayerOneCards, setPlayerTwoCards, shuffleCards, playRound, handleFlipClick, roundCounter, player1Turn, chosenAttribute, setChosenAttribute, clicked, setClicked}) => {
+const Games = ({ flip, setFlip, playerWins, isFlipped, setIsFlipped, drawArray, cards, playerOneCards, playerTwoCards, setPlayerOneCards, setPlayerTwoCards, shuffleCards, playRound, handleFlipClick, roundCounter, player1Turn, chosenAttribute, setChosenAttribute, clicked, setClicked }) => {
 
     shuffleCards(cards)
 
@@ -33,33 +33,33 @@ const Games = ({ flip, setFlip, playerWins, isFlipped, setIsFlipped, drawArray, 
     // let tempClicked = false
 
     const handleClick = (event) => {
-        if (!clicked){
+        if (!clicked) {
             setClicked(true)
 
-        const attributeSelection = event.target.id
-        console.log(attributeSelection)
+            const attributeSelection = event.target.id
+            console.log(attributeSelection)
 
-        setChosenAttribute(databaseAttributeTranslator(attributeSelection))
- 
-        setFlip(1)
+            setChosenAttribute(databaseAttributeTranslator(attributeSelection))
 
-        setTimeout(() => {
-            const tempPlayerOneCards = [...playerOneCards]
-            const tempPlayerTwoCards = [...playerTwoCards]
-            playRound(tempPlayerOneCards, tempPlayerTwoCards, attributeSelection)
+            setFlip(1)
 
-            setPlayerOneCards(tempPlayerOneCards)
-            setPlayerTwoCards(tempPlayerTwoCards)
+            setTimeout(() => {
+                const tempPlayerOneCards = [...playerOneCards]
+                const tempPlayerTwoCards = [...playerTwoCards]
+                playRound(tempPlayerOneCards, tempPlayerTwoCards, attributeSelection)
 
-        }, 4000)
-    }
+                setPlayerOneCards(tempPlayerOneCards)
+                setPlayerTwoCards(tempPlayerTwoCards)
+
+            }, 4000)
+        }
     };
 
     const cardItems1 = playerOneCards.map((card, index) => {
-        return <CardItem player1Turn={player1Turn} playerWins={playerWins} isFlipped={isFlipped} setIsFlipped={setIsFlipped} card={card} key={index} handleClick={handleClick} chosenAttribute={chosenAttribute} clicked={clicked}/>
+        return <CardItem player1Turn={player1Turn} playerWins={playerWins} isFlipped={isFlipped} setIsFlipped={setIsFlipped} card={card} key={index} handleClick={handleClick} chosenAttribute={chosenAttribute} clicked={clicked} />
     })
     const cardItems2 = playerTwoCards.map((card, index) => {
-        return <Player2CardItem setClicked={setClicked} player1Turn={player1Turn} flip={flip} setFlip={setFlip} isFlipped={isFlipped} setIsFlipped={setIsFlipped} card={card} key={index} chosenAttribute={chosenAttribute} clicked={clicked}/>
+        return <Player2CardItem setClicked={setClicked} player1Turn={player1Turn} flip={flip} setFlip={setFlip} isFlipped={isFlipped} setIsFlipped={setIsFlipped} card={card} key={index} chosenAttribute={chosenAttribute} clicked={clicked} />
     })
 
     const numberOfCards = (array) => {
@@ -68,20 +68,24 @@ const Games = ({ flip, setFlip, playerWins, isFlipped, setIsFlipped, drawArray, 
 
     return (
         <>
+            <div className="round-move-display">
+                <div className="round">
+                    {/* <h3 className="roundItems1">{chosenAttribute}</h3> */}
+                    {player1Turn ? <h3 className="roundItems1"> {chosenAttribute}</h3> : <h3 className='roundItems1'></h3>}
+                    <h3 className="roundItems2">Round: {roundCounter}</h3>
+                    {/* <h3 className="roundItems3">{chosenAttribute}</h3> */}
+                    {!player1Turn ? <h3 className="roundItems3"> {chosenAttribute}</h3> : <h3 className='roundItems3'></h3>}
+                </div>
+            </div>
             <div className="games-block">
-                <div className="player-choice-block">
+                {/* <div className="player-choice-block">
                     {player1Turn ? <h3 id='selected-card'>Player One Selected: {chosenAttribute}</h3> : <h3 id='selected-card'></h3>}
                     {!player1Turn ? <h3 id='selected-card'>Computer Selected: {chosenAttribute}</h3> : <h3 id='selected-card'></h3>}
-                </div>
-
+                </div> */}
                 <div className="card-box">
                     <div className="card-item">
                         <h1 className="player-name">Player 1</h1>
                         <b className="player-1-card" >{cardItems1[0]}</b>
-                    </div>
-                    <div className="round">
-                    <h3>Round</h3>
-                    <h1>{roundCounter}</h1>
                     </div>
                     <div className="card-item">
                         <h1 className="player-name">Player 2</h1>
@@ -91,13 +95,13 @@ const Games = ({ flip, setFlip, playerWins, isFlipped, setIsFlipped, drawArray, 
             </div>
             <div className="card-counters">
                 {/* <div className="card-counter-item"> */}
-                    <b>Cards left: {numberOfCards(cardItems1)}</b>
+                <b>Cards left: {numberOfCards(cardItems1)}</b>
                 {/* </div> */}
                 {/* <div className="card-counter-item"> */}
-                    <h3>Draw Pile: {numberOfCards(drawArray)}</h3>
+                <h3>Draw Pile: {numberOfCards(drawArray)}</h3>
                 {/* </div> */}
                 {/* <div className="card-counter-item"> */}
-                    <b>Cards left: {numberOfCards(cardItems2)}</b>
+                <b>Cards left: {numberOfCards(cardItems2)}</b>
                 {/* </div> */}
             </div>
 
